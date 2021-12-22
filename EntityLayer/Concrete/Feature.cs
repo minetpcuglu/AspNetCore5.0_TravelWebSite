b45323d3b249bdb4d314990ccc37dc9857a47049
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-22 22:38:42 +0300

    22.12 / EntityAdded

---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+   public class Feature
+    {
+        public int FeatureId { get; set; }
+        public string Post1Name { get; set; }
+        public string Post1Description { get; set; }
+        public string Post1Image { get; set; }
+        public bool Post1Status { get; set; }
+    
+    }
+}
